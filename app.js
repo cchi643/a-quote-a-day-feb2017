f1d5b258aaba39d245c68fe460c73e62bdf36333
Carlos Chi <cchi@belizetelemedia.net>
2017-02-15 14:27:32 -0600

    ccc

--- app.js
+++ app.js
@@ -5,16 +5,14 @@ var express = require('express'); //web application frame work, also called expr
 
 var app = express();
 
-app.get('/', function(req,res){
-   res.sendFile(__dirname + '/index.html');
+app.get('/', function(request,response){
+   response.sendFile(__dirname + '/index.html');
 });
 
 
-app.post('/login',function(req,res){
-    res.end("Login Successful.");
-});
 
 var port = process.env.port ||8080;
+
 var server = app.listen(port, function(reg, res){
     console.log("Server running on port 8080");// http:localhost:3000
 });
